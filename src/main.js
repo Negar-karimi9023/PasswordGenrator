6977e12062c4c52d9aff4285b65930286b4eb413
Negar Karimi <negar.skills@gmail.com>
2025-10-26 23:01:19 +0000

    Step 2 in js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+const passwordInput = document.getElementById("password");
+const passwordInput = document.getElementById("length");
+const passwordInput = document.getElementById("uppercase");
+const passwordInput = document.getElementById("lowercase");
+const passwordInput = document.getElementById("length-value");
+const passwordInput = document.getElementById("strength-label");
+const passwordInput = document.getElementById("numbers");
+const passwordInput = document.getElementById("symboles");
+const passwordInput = document.getElementById("generate-btn");
 
+const passwordInput = document.getElementById("copy-btn");
+const passwordInput = document.querySelector("copy-btn");
+const passwordInput = document.querySelector("copy-btn");
