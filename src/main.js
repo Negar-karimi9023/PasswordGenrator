bfdf46f308f678534aaee00e8eb4937b0aaaf41b
Negar Karimi <negar.skills@gmail.com>
2025-10-28 17:03:22 +0000

    updateStrengthMeter password

--- src/main.js
+++ src/main.js
@@ -1,13 +1,42 @@
+//DOM Elements
 const passwordInput = document.getElementById("password");
-const passwordInput = document.getElementById("length");
-const passwordInput = document.getElementById("uppercase");
-const passwordInput = document.getElementById("lowercase");
-const passwordInput = document.getElementById("length-value");
-const passwordInput = document.getElementById("strength-label");
-const passwordInput = document.getElementById("numbers");
-const passwordInput = document.getElementById("symboles");
-const passwordInput = document.getElementById("generate-btn");
+const lengthSlider = document.getElementById("length");
+const lengthDisplay = document.getElementById("length-value");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolesCheckbox = document.getElementById("symboles");
+const generateButton = document.getElementById("generate-btn");
+const copyButton = document.getElementById("copy-btn");
+const strengthBar = document.querySelector(".strength-bar");
+const strengthText = document.querySelector(".strength-container p");
 
-const passwordInput = document.getElementById("copy-btn");
-const passwordInput = document.querySelector("copy-btn");
-const passwordInput = document.querySelector("copy-btn");
+//Charecter sets
+
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberCharacters = "0123456789";
+const symbolCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+
+function makePassword() {
+  const length = Number(lengthSlider.value);
+  const includeUppercase = uppercaseCheckbox.checked;
+  const includeLowercase = lowercaseCheckbox.checked;
+  const includenumbers = numbersCheckbox.checked;
+  const includesymboles = symbolesCheckbox.checked;
+  if (
+    !includeUppercase &&
+    !includeLowercase &&
+    !includenumbers &&
+    !includesymboles
+  ) {
+    alert("Please select at least one char type");
+    return;
+  }
+}
+
+lengthSlider.addEventListener("input", () => {
+  lengthDisplay.textContent = lengthSlider.value;
+});
+
+generateButton.addEventListener("click", makePassword);
